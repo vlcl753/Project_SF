f4524139f67911cf6e15cf739e9b8f0bd39aa219
vlcl753 <hello.git.vlcl@naver.com>
2023-09-15 00:21:37 +0900

    main화면 구현

--- app/src/main/java/com/example/sfproject/MainActivity.java
+++ app/src/main/java/com/example/sfproject/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.sfproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
