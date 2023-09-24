f6d9db8b1b401d69bba28748b134e3109f635255
JinTaekLim <jintaek019@gmail.com>
2023-09-24 13:53:12 +0900

    Profile UI img_setting onClick 생성

--- app/src/main/java/com/example/sfproject/ProfileActivity.java
+++ app/src/main/java/com/example/sfproject/ProfileActivity.java
@@ -2,7 +2,9 @@ package com.example.sfproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void img_setting_click(View v){
+        Intent intent = new Intent(ProfileActivity.this, TestActivity.class);
+        startActivity(intent);
     }
 }
