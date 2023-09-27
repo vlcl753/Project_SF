2e93f479a462b3a26ba91f4c505991c51b1db7b6
JinTaekLim <jintaek019@gmail.com>
2023-09-27 11:42:33 +0900

    Post_Create UI 초안 생성

---
+++ app/src/main/java/com/example/sfproject/Post_CreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.sfproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Post_CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_create);
+    }
+}
