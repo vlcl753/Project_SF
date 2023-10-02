e06127ad9be04420e712add7d16c0c6b46fe636d
JinTaekLim <jintaek019@gmail.com>
2023-10-02 21:22:28 +0900

    데이터베이스 연동(build.gradle파일 수정) , Activity_test 데이터베이스 내용 가져오기 예제

--- app/src/main/java/com/example/sfproject/TestActivity.java
+++ app/src/main/java/com/example/sfproject/TestActivity.java
@@ -1,14 +1,16 @@
 package com.example.sfproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
+import android.util.Log;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -16,16 +18,36 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-    }
+        FirebaseApp.initializeApp(this);
 
+        // Firestore 인스턴스 생성
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("MainPost").document("MainPost-1");
 
-    public void LoginClick(View v){
-        Intent intent = new Intent(TestActivity.this, LoginActivity.class);
-        startActivity(intent);
-    }
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // 문서가 존재하는 경우 데이터 가져오기
+                        String content = document.getString("content");
+                        String date = document.getString("date");
+                        String title = document.getString("title");
+                        String userName = document.getString("userName");
 
-    public void ProflieClick(View v){
-        Intent intent = new Intent(TestActivity.this, ProfileActivity.class);
-        startActivity(intent);
+                        // TextView 업데이트
+                        TextView dataTextView = findViewById(R.id.dataTextView);
+                        dataTextView.setText("Content: " + content + "\nDate: " + date + "\nTitle: " + title + "\nUserName: " + userName);
+                    } else {
+                        // 문서가 존재하지 않는 경우 처리
+                        Log.d("FirestoreData", "문서가 존재하지 않습니다.");
+                    }
+                } else {
+                    // 오류 처리
+                    Log.d("FirestoreData", "데이터를 불러오는 중 오류가 발생했습니다.");
+                }
+            }
+        });
     }
 }
