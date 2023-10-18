360148082dec25ebe448618ca1f075c68568b157
vlcl753 <hello.git.vlcl@naver.com>
2023-10-18 16:28:41 +0900

    네이게이션바 화면전환

--- app/src/main/java/com/example/sfproject/MainActivity.java
+++ app/src/main/java/com/example/sfproject/MainActivity.java
@@ -6,12 +6,18 @@ import android.widget.ImageView;
 import android.os.Bundle;
 import android.widget.ViewFlipper;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     public void goToPostActivity(View view) {
         Intent intent = new Intent(this, PostActivity.class);
         startActivity(intent);
     }
+    private BottomNavigationView bottomNavigationView;
 
     public void goToSearchActivity(View view) {
         Intent intent = new Intent(this, SearchActivity.class);
@@ -25,10 +31,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         int images[] = {
-                R.drawable.test1,
-                R.drawable.test2,
-                R.drawable.test1
+                R.drawable.banner1,
+                R.drawable.banner2,
+                R.drawable.banner3
         };
 
         v_fllipper = findViewById(R.id.image_slide);
@@ -36,6 +43,36 @@ public class MainActivity extends AppCompatActivity {
         for(int image : images) {
             fllipperImages(image);
         }
+        bottomNavigationView = findViewById(R.id.bottom_navigationview);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId(); // 아이템 ID를 가져옵니다.
+                // 아이템 ID에 따라 액티비티를 시작합니다.
+                int itemID = item.getItemId();
+                if (itemID == R.id.home) {
+                    // 홈 아이템을 클릭했을 때 MainActivity로 이동
+                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                } else if (itemID == R.id.addbtn) {
+                    // 글쓰기 아이템을 클릭했을 때 Post_CreateActivity로 이동
+                    startActivity(new Intent(MainActivity.this, Post_CreateActivity.class));
+                } else if (itemID == R.id.notibtbn) {
+                    // 알람 아이템을 클릭했을 때 Notification으로 이동
+                    startActivity(new Intent(MainActivity.this, Notification.class));
+                } else if (itemID == R.id.setting) {
+                    // 설정 아이템을 클릭했을 때 Profile_EditActivity로 이동
+                    startActivity(new Intent(MainActivity.this, Profile_EditActivity.class));
+                }
+
+                return false;
+            }
+        });
+
+
+
+
+
     }
 
     public void fllipperImages(int image) {
