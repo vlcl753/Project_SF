31b119046784bb7a33bf99bbec17d7adf7ddf931
vlcl753 <hello.git.vlcl@naver.com>
2023-10-18 19:43:43 +0900

    네이게이션바 화면전환 추가

--- app/src/main/java/com/example/sfproject/Notification.java
+++ app/src/main/java/com/example/sfproject/Notification.java
@@ -1,14 +1,47 @@
 package com.example.sfproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Notification extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigationview);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId(); // 아이템 ID를 가져옵니다.
+                // 아이템 ID에 따라 액티비티를 시작합니다.
+                int itemID = item.getItemId();
+                if (itemID == R.id.home) {
+                    // 홈 아이템을 클릭했을 때 MainActivity로 이동
+                    startActivity(new Intent(Notification.this, MainActivity.class));
+                } else if (itemID == R.id.add) {
+                    // 글쓰기 아이템을 클릭했을 때 Post_CreateActivity로 이동
+                    startActivity(new Intent(Notification.this, Post_CreateActivity.class));
+                } else if (itemID == R.id.noti) {
+                    // 알람 아이템을 클릭했을 때 Notification으로 이동
+                    startActivity(new Intent(Notification.this, Notification.class));
+                } else if (itemID == R.id.setting) {
+                    // 설정 아이템을 클릭했을 때 Profile_EditActivity로 이동
+                    startActivity(new Intent(Notification.this, Profile_EditActivity.class));
+                } else if (itemID == R.id.search) {
+                    // 설정 아이템을 클릭했을 때 Profile_EditActivity로 이동
+                    startActivity(new Intent(Notification.this, SearchActivity.class));
+                }
+
+                return false;
+            }
+        });
     }
 }
