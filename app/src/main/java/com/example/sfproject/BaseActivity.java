64ff24def81d71c2cea76cbe91cb1e2285e128f2
JinTaekLim <jintaek019@gmail.com>
2023-10-29 22:11:34 +0900

    activity_base.xml -> 프로필 UI 보완 , 스크롤바 , 게시물 ,바텀 네비게이션바 전체적으로 설정 변경 / 자바 코드로 UI 불러오기 테스트 중

--- app/src/main/java/com/example/sfproject/BaseActivity.java
+++ app/src/main/java/com/example/sfproject/BaseActivity.java
@@ -24,11 +24,12 @@ public class BaseActivity extends AppCompatActivity {
         // CardView와 LinearLayout 동적 생성
         CardView cardView = createCardView();
         LinearLayout linearLayout = createLinearLayout();
-        ImageView imageView = createProfileImageView();
 
-        // LinearLayout에 ImageView 추가
-        linearLayout.addView(imageView);
-
+        // LinearLayout에 CardView 추가
+        linearLayout.addView(createCardView_1(R.drawable.sjg01));
+        linearLayout.addView(createCardView_1(R.drawable.sjg01));
+        linearLayout.addView(createCardView_1(R.drawable.sjg01));
+        linearLayout.addView(createCardView_1(R.drawable.sjg01));
         // 생성한 LinearLayout을 CardView에 추가
         cardView.addView(linearLayout);
 
@@ -41,35 +42,17 @@ public class BaseActivity extends AppCompatActivity {
     private static final int CARD_VIEW_RADIUS_DP = 8;
     private CardView createCardView() {
         CardView cardView = new CardView(this);
-        cardView.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        cardView.setLayoutParams(layoutParams);
 
-        // 배경색 설정
+        // 카드뷰 속성 설정
         cardView.setCardBackgroundColor(getResources().getColor(R.color.white));
-
-        // 테두리 반지름 값 설정 (픽셀 단위)
-        int cardViewRadiusPixels = (int) (CARD_VIEW_RADIUS_DP * getResources().getDisplayMetrics().density);
-        cardView.setRadius(cardViewRadiusPixels);
-
-        // 그림자 설정
-        cardView.setCardElevation(0);
-
-        // CardView에 ID 설정
-        cardView.setId(R.id.profile_post);
-
-        // CardView 내부에 추가할 LinearLayout 생성 및 설정
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
-        // 여기에 원하는 레이아웃 구성 요소들을 추가할 수 있습니다.
-
-        // CardView에 LinearLayout 추가
-        cardView.addView(linearLayout);
+        cardView.setCardElevation(0); // 그림자 높이 설정
 
         return cardView;
     }
+
 
     private LinearLayout createLinearLayout() {
         LinearLayout linearLayout = new LinearLayout(this);
@@ -80,6 +63,42 @@ public class BaseActivity extends AppCompatActivity {
 
         return linearLayout;
     }
+
+    private CardView createCardView_1(int imageResId) {
+        CardView cardView = new CardView(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                dpToPixels(105), dpToPixels(105)); // 너비와 높이를 dp 단위로 설정
+        layoutParams.setMargins(dpToPixels(10), dpToPixels(10), dpToPixels(10), dpToPixels(10)); // 마진을 dp 단위로 설정
+        cardView.setLayoutParams(layoutParams);
+
+        // 카드뷰 속성 설정
+        cardView.setCardElevation(0); // 그림자 높이 설정
+        cardView.setRadius(dpToPixels(20)); // 테두리의 곡률 설정
+
+        // ImageView 생성 및 설정
+        ImageView imageView = new ImageView(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
+        imageView.setLayoutParams(params);
+        imageView.setImageResource(imageResId);
+        imageView.setClickable(true);
+        imageView.setFocusable(true);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        // ImageView를 CardView에 추가
+        cardView.addView(imageView);
+
+        return cardView;
+    }
+
+    // dp 값을 픽셀로 변환하는 메서드
+    private int dpToPixels(float dp) {
+        float scale = getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+
 
     private static final int CARD_VIEW_MARGIN_DP = 8;
     private ImageView createProfileImageView() {
