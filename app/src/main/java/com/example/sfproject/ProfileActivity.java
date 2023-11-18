ecc00c2f3656a709bfa45eb4db64badb609f6648
JinTaekLim <jintaek019@gmail.com>
2023-11-18 20:48:54 +0900

    ProfileActivity 팔로우,팔로잉 데이터 적용

--- app/src/main/java/com/example/sfproject/ProfileActivity.java
+++ app/src/main/java/com/example/sfproject/ProfileActivity.java
@@ -18,8 +18,11 @@ import androidx.cardview.widget.CardView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -42,6 +45,9 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String COLLECTION_NAME = "Profile";
     String USER_UID = "ikZZTQIEEAetiZgPSFumXU1Cv3I3";
 
+    private TextView Profile_follow_num;
+    private TextView Profile_following_num;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +59,34 @@ public class ProfileActivity extends AppCompatActivity {
         firestore = FirebaseFirestore.getInstance();
         // 데이터 갯수 가져오기
         getTotalItemsCount();
+
+
+        Profile_follow_num = findViewById(R.id.textView_follow_num);
+        Profile_following_num = findViewById(R.id.textView_following_num);
+
+        DocumentReference follow_dc = firestore.collection("Profile").document(USER_UID);
+
+        follow_dc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // 문서 데이터에서 필요한 값을 가져오기
+                        String follow_num = String.valueOf(document.getLong("follow"));
+                        String following_num = String.valueOf(document.getLong("following"));
+                        // 필요한 작업 수행
+                        Profile_follow_num.setText(follow_num);
+                        Profile_following_num.setText(following_num);
+                    } else {
+                        // 문서가 없는 경우 처리
+                    }
+                } else {
+                    // 오류 처리
+                }
+            }
+        });
+
 
         profile_editbtn = findViewById(R.id.profile_editbtn);
         profile_editbtn.setOnClickListener(new View.OnClickListener() {
